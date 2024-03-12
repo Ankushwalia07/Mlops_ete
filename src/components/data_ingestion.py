901c57fa7a04f7f50294c27ff32b7605c9365f67
Jaimaamusiccenter <Waliaankush999@gmail.com>
2024-03-12 11:28:30 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path : str = os.path.join('artifacts', "train.csv")
@@ -18,7 +22,7 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
-    def intiate_data_ingestion(self):
+    def initiate_data_ingestion(self):
         logging.info("Entered the data Ingestiin Mehtod")
         try:
             df = pd.read_csv("notebook/data/stud.csv")
@@ -46,7 +50,9 @@ class DataIngestion:
         
 
 if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.intiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
