502110468e0a4cacef7cf0a6bf6d0a41742cb44e
Jaimaamusiccenter <Waliaankush999@gmail.com>
2024-03-19 12:06:24 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,14 +1,19 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
+
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+from src.exception import CustomException
+from src.logger import logging
 
 
 @dataclass
@@ -55,4 +60,8 @@ if __name__ == "__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+    
 
